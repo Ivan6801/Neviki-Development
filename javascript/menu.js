fc26d27d4ef52b053f1817ba53d95802f2edef1a
Ivan Gonzalez <hal308366@gmail.com>
2021-09-24 10:31:26 -0700

    Hacer el burger de menú

--- javascript/menu.js
+++ javascript/menu.js
@@ -6,9 +6,9 @@ console.log('Diseño css burder menu', menu);
 const hideContainer = document.querySelector('.hide-container');
 console.log('Diseño hide container', hideContainer);
 
-const burgerButton = document.querySelector('.burger-button');
-console.log('Menu:', burgerButton);
-
+// Perfil menu
+const perfilMenu = document.querySelector('.perfil-menu');
+console.log('Perfil menu:', perfilMenu);
 
 
 function hideShow() {
@@ -16,6 +16,22 @@ function hideShow() {
         menu.classList.remove('is-active');
     } else {
         menu.classList.add('is-active');
+    }
+
+    if (hideContainer.classList.contains('is-hide')) {
+        hideContainer.classList.remove('is-hide');
+    } else {
+        hideContainer.classList.add('is-hide');
+    }
+}
+
+
+
+function hideShowPerfil() {
+    if (perfilMenu.classList.contains('is-perfil')) {
+        perfilMenu.classList.remove('is-perfil');
+    } else {
+        perfilMenu.classList.add('is-perfil');
     }
 
     if (hideContainer.classList.contains('is-hide')) {
